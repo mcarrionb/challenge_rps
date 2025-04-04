8532b75ea24e6b993a1de7855d63efd8f926d2f8
Mar Carrion <mar.carrion.borras@gmail.com>
2025-04-04 19:54:13 +0200

    Toggle button

--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -10,22 +10,11 @@ export const WelcomePage: React.FC = () => {
   };
 
   return (
-    <div className="welcome-container">
+    <div className="rounded-md border border-gray-300 p-4 shadow-sm sm:p-6 welcome-container">
       <div className="overlay">
         <h2 className="welcome-title">WELCOME TO</h2>
         <h1 className="game-title">ROCK, PAPER, SCISSORS</h1>
-{/*         <button onClick={handleStart} className="start-button">START GAME</button>
- */}        <a className="group relative inline-block focus:ring-3 focus:outline-hidden" href="#">
-          <span
-            className="absolute inset-0 translate-x-1.5 translate-y-1.5 bg-violet-300 transition-transform group-hover:translate-x-0 group-hover:translate-y-0"
-          ></span>
-
-          <span onClick={handleStart}
-            className="relative inline-block border-2 border-current px-8 py-3 text-sm font-bold tracking-widest text-black uppercase"
-          >
-            START GAME
-          </span>
-        </a>
+        <button onClick={handleStart} className="start-button">START GAME</button>
       </div>
     </div>
 
